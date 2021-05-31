cbd1d9e386f6fbc96fc05be039c5eba71438b942
Carlos Quadros <50021330+CarlosAQuadros@users.noreply.github.com>
2021-05-30 22:04:34 -0300

    added styled components

--- App.tsx
+++ App.tsx
@@ -1,19 +1,6 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Dashboard } from './src/screens/Dashboard';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>expo</Text>
-    </View>
-  );
+  return<Dashboard/>;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#ff0000',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/screens/Dashboard.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export function Dashboard() { 
+    return(
+  <View style={styles.container}>
+      <Text>
+        React Native
+      </Text>
+  </View>
+    ) 
+}
+
+const styles =StyleSheet.create({
+        container:{
+            flex:1,
+            justifyContent:'center',
+            alignItems:'center',
+            backgroundColor:'white',
+            margin:10
+        }
+
+
+})
+
+   
