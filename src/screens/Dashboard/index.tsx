f279a21c03be701139df901473cc6a3a19fc261e
Carlos Quadros <50021330+CarlosAQuadros@users.noreply.github.com>
2021-06-05 17:13:19 -0300

    aleatoy commit

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,31 @@
 import React from 'react';
-import { Container, Title } from './styles';
+import {
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  UserImage,
+  User,
+  UserGreetingg,
+  UserName
+} from './styles';
 export function Dashboard() {
   return (
     <Container >
-      <Title>
-        React Native
-      </Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <UserImage
+              source={{ uri: 'https://avatars.githubusercontent.com/u/50021330?v=4' }} >
+            </UserImage>
+            <User>
+              <UserGreetingg>Olá,</UserGreetingg>
+              <UserName>Carlos</UserName>
+            </User>
+          </UserInfo>
+        </UserWrapper>
+
+      </Header>
     </Container>
   )
 }
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,18 +1,47 @@
 import styled from 'styled-components/native';
+import { RFPercentage, RFValue } from 'react-native-responsive-fontsize'
+import theme from '../../global/styles/theme';
 
 export const Container = styled.View`
     flex: 1;
-    justify-content: center;
+    background-color: ${({ theme }) => theme.colors.background};
+
+`;
+
+export const Header = styled.View`
+    height: ${RFPercentage(42)}px;
+    background-color: ${({ theme }) => theme.colors.primary};
+   
+`;
+export const UserWrapper = styled.View`
+    padding: 0 25px;
+    width: 100%;
+    margin-top: 26px;
+    background-color: yellowgreen;
+`;
+
+export const UserInfo = styled.View`
+ 
+    flex-direction: row;
     align-items: center;
-    background-color: ${({theme }) => theme.colors.background};
+   
 
-`;
-
-export const Title = styled.Text`
-    font-family:${({theme})=> theme.fonts.bold} ;
-    font-size: 24px;
-    
-    color: ${({ theme }) => theme.colors.title};
-    
-
-`;
+ `;
+export const UserImage = styled.Image`
+ width: ${RFValue(48)}px;
+ height: ${RFValue(52)}px;
+ border-radius: 10px;
+ `;
+export const User = styled.View`
+ margin-left: 17px;
+ `;
+export const UserGreetingg = styled.Text`
+ color: ${({ theme }) => theme.colors.shape};
+ font-size:${RFValue(18)}px;
+ font-family: ${({ theme }) => theme.fonts.regular};
+ `;
+export const UserName = styled.Text`
+ font-size:${RFValue(18)}px;
+ font-family: ${({ theme }) => theme.fonts.bold};
+  color: ${({ Theme }) => theme.colors.shape};
+ `;
